9fc6166eb7bd06dd4c12f71f3ce52149003bb72b
Lewis Coates <lewis.coates@intel.com>
2017-02-14 10:32:38 -0800

    Test Random forest enhancements (#6)
    * Adds tests for random forest in the case of a very deep (> 32 layers) tree, also tests feature importance and minimum row split
    
    * sorting metadata
    
    * fixing documentation

---
+++ regression-tests/sparktkregtests/testcases/models/random_forest_deep_test.py
@@ -0,0 +1,52 @@
+# vim: set encoding=utf-8
+
+#  Copyright (c) 2016 Intel Corporation 
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+""" Tests the random forest functionality """
+import unittest
+from sparktkregtests.lib import sparktk_test
+
+
+class RandomForestDeep(sparktk_test.SparkTKTestCase):
+
+    def setUp(self):
+        """Build the required frame"""
+        super(RandomForestDeep, self).setUp()
+
+        schema = [("feat"+str(i), int) for i in range(9)] + [("class", int)]
+        filename = self.get_file("rand_forest_regress.csv")
+
+        self.frame = self.context.frame.import_csv(filename, schema=schema)
+
+    def test_deep_regression_tree(self):
+        """ Test the regression model, forcing the model deeper than 32"""
+        schema = ["feat"+str(i) for i in range(9)]
+        model32 = self.context.models.regression.random_forest_regressor.train(
+            self.frame, schema, "class", max_depth=32, seed=0)
+
+        model34 = self.context.models.regression.random_forest_regressor.train(
+            self.frame, schema, "class", max_depth=34, seed=0)
+        result32 = model32.test(self.frame)
+        result34 = model34.test(self.frame)
+        # This result is determined experimentally, it was better than when
+        # this test is run with a depth of 32. We use a fixed value in the
+        # interest of expedience, this test is already quite long
+        self.assertGreater(
+            result32.mean_squared_error, result34.mean_squared_error)
+
+
+if __name__ == '__main__':
+    unittest.main()
